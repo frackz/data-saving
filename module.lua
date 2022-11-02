8d3917edfdd235b3fda55d2f557bd382346ca45d
frackz <65610875+frackz@users.noreply.github.com>
2022-11-02 21:09:44 +0100

    Create module.lua

---
+++ module.lua
@@ -0,0 +1,64 @@
+local module = {}
+
+local Players = game:GetService("Players")
+
+local DataStoreName = "UserData"
+
+local DataStoreService = game:GetService("DataStoreService")
+local DataStore = DataStoreService:GetDataStore(DataStoreName)
+
+local Funcs = {
+	['Added'] = {},
+	['Removed'] = {}
+}
+
+Players.PlayerAdded:Connect(function(Player)
+	local Handler = {}
+	Handler.New = function(
+		name : string,
+		path : Instance,
+		instance : string,
+		value : any
+	)
+		local NewInstance = Instance.new(instance, path)
+		NewInstance.Name = name
+		NewInstance.Value = value
+		return NewInstance
+	end
+	
+	local Success, Data = pcall(function()
+		return DataStore:GetAsync(Player.UserId)
+	end)
+	
+	if not Success then warn(Data) Data = {} end
+	if Data == nil then Data = {} end
+	for i,v in pairs(Funcs.Added) do
+		v(Player, Data, Handler)
+	end
+end)
+
+Players.PlayerRemoving:Connect(function(Player)
+	local Data = {}
+	local Set = function(key,val) Data[key] = val end
+	
+	for i,v in pairs(Funcs.Removed) do
+		v(Player, Set)
+	end
+	
+	local Success, Error = pcall(function()
+		DataStore:SetAsync(Player.UserId, Data)
+	end)
+	if not Success then
+		return warn(Error)
+	end
+end)
+
+module.Added = function(func)
+	table.insert(Funcs.Added, func)
+end
+
+module.Removed = function(func)
+	table.insert(Funcs.Removed, func)
+end
+
+return module
